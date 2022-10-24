3fa1b672e62ffbefe69fc9106b636ea35b03bf3b
BakaFT <1355564740@qq.com>
2022-10-25 01:08:01 +0800

    Fix: install missing npm package

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,6 +15,7 @@ git config --global user.email "$INPUT_USER_EMAIL"
 # install hexo env
 npm install hexo-cli -g
 npm install hexo-deployer-git --save
+npm install --save hexo-pagination@^2.0.0
 
 # deployment
 if [ "$INPUT_COMMIT_MSG" = "none" ]
